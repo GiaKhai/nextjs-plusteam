be5c5af9a4a724cad1e0ddba8987e6087d0c450b
GiaKhai <khaigia11111@gmail.com>
2021-05-20 18:04:40 +0700

    update logo

---
+++ components/candidate.js
@@ -0,0 +1,128 @@
+/** @format */
+
+export default function Candidate() {
+  return (
+    <section className="section area-padding-top ">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h4 className="section-title text-center">
+              Crypto Payment Allowance
+            </h4>
+            <p className="text-sumary">
+              We accepted payment via Crypto currency USDT, BNB, BTC, ETH
+            </p>
+            <p></p>
+          </div>
+          <div className="col-12">
+            <div style={{ width: "80%", margin: "auto" }}>
+              <div className="col-12 margin-t-50">
+                <div
+                  id="carouselExampleSlidesOnly"
+                  className="carousel slide"
+                  data-ride="carousel"
+                >
+                  <div className="carousel-inner">
+                    <div className="carousel-item active">
+                      <div className="row">
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img src="/static/img/Candidate/shoppe.svg" alt="" />
+                        </div>
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img src="/static/img/Candidate/lazada.svg" alt="" />
+                        </div>
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img src="/static/img/Candidate/grab.svg" alt="" />
+                        </div>
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img
+                            src="/static/img/Candidate/microsoft.svg"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="carousel-item">
+                      <div className="row">
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img src="/static/img/Candidate/shoppe.svg" alt="" />
+                        </div>
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img src="/static/img/Candidate/lazada.svg" alt="" />
+                        </div>
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img src="/static/img/Candidate/grab.svg" alt="" />
+                        </div>
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img
+                            src="/static/img/Candidate/microsoft.svg"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="carousel-item">
+                      <div className="row">
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img src="/static/img/Candidate/shoppe.svg" alt="" />
+                        </div>
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img src="/static/img/Candidate/lazada.svg" alt="" />
+                        </div>
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img src="/static/img/Candidate/grab.svg" alt="" />
+                        </div>
+                        <div
+                          className="col-lg-3"
+                          style={{ textAlign: "center", margin: "10px 0" }}
+                        >
+                          <img
+                            src="/static/img/Candidate/microsoft.svg"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
